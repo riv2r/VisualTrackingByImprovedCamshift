e7933dddbb396a7641f5fdcbf0290a380b047913
riv2r <3237208018@qq.com>
2021-03-27 23:38:05 +0800

    加入kalman滤波器 优化算法1.0

--- OBJTracking.py
+++ OBJTracking.py
@@ -6,21 +6,41 @@ import time
 mtx = np.loadtxt('cameraArgs/mtx.txt', delimiter=',')
 dist = np.loadtxt('cameraArgs/dist.txt', delimiter=',')
 
-# 初始化全局变量
-xs, ys, ws, hs = 0, 0, 0, 0  # 初始化鼠标选取区域左上角坐标和长宽
-xo, yo = 0, 0  # 初始化左上角原始坐标
-selectObject = False  # 标记是否选取了目标
-trackObject = 0  # 1 表示有追踪对象 0 表示无追踪对象 -1 表示追踪对象尚未计算 Camshift 所需的属性
-trackPath = []  # 轨迹路径列表
-predictTrackPath= [] # 预测路径列表
+# 初始化鼠标选取区域左上角坐标和长宽
+xs, ys, ws, hs = 0, 0, 0, 0
+# 初始化左上角原始坐标
+xo, yo = 0, 0
+# 标记是否选取了目标
+selectObject = False
+# 1 表示有追踪对象 0 表示无追踪对象 -1 表示追踪对象尚未计算 Camshift 所需的属性
+trackObject = 0
+# 轨迹路径列表
+trackPath = []
 
 # kalman滤波器初始化
-kalman = cv2.KalmanFilter(4, 2) # 4 状态数 包括(x,y,dx,dy)坐标及速度(每次移动的距离) 2 观测量 能看到的是坐标值
-kalman.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32) # 系统测量矩阵
-kalman.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32) # 状态转移矩阵
-kalman.processNoiseCov = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)*0.03 # 系统过程噪声协方差
-last_measurement = current_measurement = np.array((2, 1), np.float32)
-last_prediction = current_prediction = np.zeros((2, 1), np.float32)
+# 4 状态数 包括(x,y,dx,dy)坐标及速度(每次移动的距离) 2 观测量 表示能测量到的值
+kalman = cv2.KalmanFilter(4, 2)
+# 转移矩阵A
+kalman.transitionMatrix = np.array([[1, 0, 1, 0],
+                                    [0, 1, 0, 1],
+                                    [0, 0, 1, 0],
+                                    [0, 0, 0, 1]], np.float32)
+# 过程噪声协方差矩阵Q
+kalman.processNoiseCov = np.array([[1, 0, 0, 0],
+                                   [0, 1, 0, 0],
+                                   [0, 0, 1, 0],
+                                   [0, 0, 0, 1]], np.float32) * 1e-5
+# 测量矩阵H
+kalman.measurementMatrix = np.array([[1, 0, 0, 0],
+                                     [0, 1, 0, 0]], np.float32)
+# 测量噪声协方差矩阵R
+kalman.measurementNoiseCov = np.array([[1, 0],
+                                       [0, 1]], np.float32) * 1e-1
+# 最优估计值statePost
+kalman.statePost = np.float32(np.random.randn(4, 1))
+# 最小均方误差矩阵P
+kalman.errorCovPost = np.ones((4, 4), np.float32)
+
 
 # 鼠标回调函数
 def onMouse(event, x, y, flags, prams):
@@ -47,16 +67,18 @@ def onMouse(event, x, y, flags, prams):
 
 # 处理算法函数
 def OBJTracking():
-    global xs, ys, ws, hs, selectObject, xo, yo, trackObject, trackPath, last_measurement, last_prediction, current_measurement, current_prediction
-
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # 捕获摄像头
+    global xs, ys, ws, hs, selectObject, xo, yo, trackObject, trackPath
+    # 捕获摄像头
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     cv2.namedWindow('CamshiftOBJTracking')
-    cv2.setMouseCallback('CamshiftOBJTracking', onMouse)  # 调用鼠标回调函数
+    # 调用鼠标回调函数
+    cv2.setMouseCallback('CamshiftOBJTracking', onMouse)
     # 设置结束标志，10 次迭代或至少 1 次移动
     term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
 
     while (True):
-        ret, frame = cap.read()  # 连续读取视频帧
+        # 连续读取视频帧
+        ret, frame = cap.read()
         h, w = frame.shape[:2]
         newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
         # 校准图像
@@ -65,55 +87,58 @@ def OBJTracking():
         # 裁剪图像
         x, y, w, h = roi
         frame = frame[y:y + h, x:x + w]
-        
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # 将BGR转换为HSV空间
-        mask = cv2.inRange(hsv, np.array((0., 30., 10.)), np.array((180., 256., 255.)))  # 制作掩模版
-        
+        # 将BGR转换为HSV空间
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # 制作掩模版
+        mask = cv2.inRange(hsv, np.array((0., 43., 46.)), np.array((180., 255., 255.)))
+
         # 表示已捕获跟踪对象
         if trackObject != 0:
+            # 计算camshift所需属性
             if trackObject == -1:
                 track_window = (xs, ys, ws, hs)
                 mask_roi = mask[ys:ys + hs, xs:xs + ws]
                 hsv_roi = hsv[ys:ys + hs, xs:xs + ws]
                 # 构建图像分布直方图
-                roi_hist = cv2.calcHist([hsv_roi], [0], mask_roi, [181], [0, 180])
+                roi_hist = cv2.calcHist([hsv_roi], [0], mask_roi, [180], [0, 180])
                 # 归一化处理
                 cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
                 trackObject = 1
             # 反向投影
-            dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
-            dst &= mask
+            backProj = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+            backProj &= mask
+
             # 调用CAMshift算法
-            ret, track_window = cv2.CamShift(dst, track_window, term_crit)
-
+            ret, track_window = cv2.CamShift(backProj, track_window, term_crit)
             x, y, w, h = track_window
-            # 插入卡尔曼滤波算法
-            last_prediction = current_prediction # 把当前预测存储为上一次预测
-            last_measurement = current_measurement # 把当前测量存储为上一次测量
-            current_measurement = np.array([[np.float32(x+w//2)], [np.float32(y+h//2)]]) # 当前测量
-            kalman.correct(current_measurement) # 用当前测量来校正卡尔曼滤波器
-            current_prediction = kalman.predict() # 计算卡尔曼预测值，作为当前预测
-            cmx, cmy = int(current_measurement[0]), int(current_measurement[1]) # 当前测量坐标
-            cpx, cpy = int(current_prediction[0]), int(current_prediction[1]) # 当前预测坐标
-
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            # 卡尔曼滤波_预测
+            statePre = kalman.predict()
+            # 卡尔曼滤波_更新
+            measurement = np.array([[x + w // 2],
+                                    [y + h // 2]], np.float32)
+            kalman.correct(measurement)
+            # 获取校正后的位置坐标
+            px, py = int(kalman.statePost[0]), int(kalman.statePost[1])
+            # 绘制捕获方框
+            cv2.rectangle(frame, (px - w // 2, py - h // 2), (px + w // 2, py + h // 2), (255, 0, 0), 2)
             # 记录时间
             now = time.time()
-            trackPath.append([cmx, cmy, now])
-            predictTrackPath.append([cpx, cpy, now])
+            trackPath.append([px, py, now])
 
-            for i in range(21, len(trackPath)):
-                cv2.line(frame, (trackPath[i - 1][0], trackPath[i - 1][1]), (trackPath[i][0], trackPath[i][1]), (0, 255, 0), 2)
-            for i in range(21, len(predictTrackPath)):
-                cv2.line(frame, (predictTrackPath[i - 1][0], predictTrackPath[i - 1][1]), (predictTrackPath[i][0], predictTrackPath[i][1]), (255, 0, 0), 2)
-        
+            for i in range(1, len(trackPath)):
+                cv2.line(frame, (trackPath[i - 1][0], trackPath[i - 1][1]), (trackPath[i][0], trackPath[i][1]),
+                         (0, 255, 0), 2)
+
+        # 显示鼠标选择区域
         if selectObject and ws > 0 and hs > 0:
             cv2.bitwise_not(frame[ys:ys + hs, xs:xs + ws], frame[ys:ys + hs, xs:xs + ws])
 
+        # 显示图像
         cv2.imshow('CamshiftOBJTracking', frame)
 
+        # 控制程序停止
         key = cv2.waitKey(10)
-        if key == 27:
+        if key == 27 or key == ord('q'):
             break
 
     cv2.destroyAllWindows()
