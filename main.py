5a598d73fa1e80812323fb179a0790b391c3c73a
riv2r <3237208018@qq.com>
2021-05-17 22:45:27 +0800

    补充main.py注释

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import os
 from cameraCalib import *
 
+'''
+需要补充如下文件夹及文件
+1.__pycache__:缓存文件夹 内部缓存文件 可自动生成
+2..vscode:运行环境配置文件夹
+3.calibPics:相机标定素材图片文件夹 内部10张相机标定素材图片
+4.chessboard:黑白棋盘格文件夹 内部黑白棋盘word和png版
+5.video:标定视频素材文件夹 内部test.mp4
+'''
+
 if os.path.exists('cameraArgs/mtx.txt') and os.path.exists('cameraArgs/dist.txt'):
     pass
 else:
