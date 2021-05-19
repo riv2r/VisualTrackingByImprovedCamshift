9593b4fbff5519c4a6cda800a253147f29d4c529
riv2r <3237208018@qq.com>
2021-05-19 13:50:30 +0800

    更改时间序列

--- getImgPoints.py
+++ getImgPoints.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def getImgPoints(trackPath):
     trackPath = np.mat(trackPath).reshape(-1, 3)
+    trackPath[:,2] = trackPath[:,2]-trackPath[0,2]
     np.savetxt('data/ImgPoints.txt', trackPath, fmt='%f', delimiter=',')
 
 
